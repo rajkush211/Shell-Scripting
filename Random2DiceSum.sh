dcb2b673dda8e82e6a2691727a33e97c8a79901d
rajkush211 <59891604+rajkush211@users.noreply.github.com>
2020-03-11 14:27:02 +0530

    Update Random2DiceSum.sh

--- Random2DiceSum.sh
+++ Random2DiceSum.sh
@@ -3,8 +3,9 @@
 #Problem Statement :- Add two Random Dice Number and Print the Result
 #Author :- Raj Kush
 #Date :- 09 March 2020
-
+clear
 random1=$(( RANDOM % 6 + 1 ))
 random2=$(( RANDOM % 6 + 1 ))
 
 echo "Addition of two Random Numbers is $((random1 + random2 ))"
+sleep 2
