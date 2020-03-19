076e64f843572def6503832071e335756357d298
rajkush211 <rajkush211.rk@gmail.com>
2020-03-20 03:36:24 +0530

    ADDED: Program to find individuals whose birthday in the same month

---
+++ Dictionary/sameBirthdaymonth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -X
+
+clear
+
+TOTAL_PERSONS=50
+MONTHS=12
+
+for (( person=1; person<=$TOTAL_PERSONS; person++ ))
+do
+	birthMonth=$(( RANDOM % MONTHS + 1 ))
+	birthdays[birthMonth]=$(( ${birthdays[birthMonth]} + 1 ))
+done
+
+for (( month=1; month<=$MONTHS; month++ ))
+do
+	echo "People who have Birthday in month $month: ${birthdays[month]}" 
+done
+
+sleep 1
